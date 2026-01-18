420886caf80e720bcb10ecee07a6bba51724e550
iamlusca <lucasfelipelima10@gmail.com>
2026-01-18 19:29:07 -0300

    atualização do código

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-console.log ("teste")
+let jogoPs5 = ["EA FC 26", "Red Dead 2", "Cyberpunk"]
+
+let infoJogos = [
+    ["EA FC 26", "15+", "Nota no Metacritic 7,5"],
+    ["Red Dead 2", "18+", "Nota no Metacritic 10"],
+    ["Cyberpunk", "18+", "Nota no Metacritic 7"]
+]
+
+console.log( " O jogo " + infoJogos[1][0] + " tem " + infoJogos[1][2] + " e indicação de idade " + infoJogos[1][1]),
+console.log( " O jogo " + infoJogos[0][0] + " tem " + infoJogos[0][2] + " e indicação de idade " + infoJogos[0][1]),
+console.log( " O jogo " + infoJogos[2][0] + " tem " + infoJogos[2][2] + " e indicação de idade " + infoJogos[2][1])
 
